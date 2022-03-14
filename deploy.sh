7c6f95f364365e0bbfd9c2a34271fc53a8123304
yangyang.li <yangyang.li@feiniu.com>
2022-03-14 10:01:33 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ set -e
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/Young1Lee/young1lee.github.io.git master:main
+git push https://github.com/Young1Lee/young1lee.github.io.git master:gh-pages
 
 cd -
